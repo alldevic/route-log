09b4fcd0a88575c4c0af2f52c86bd0fc4e2c5e33
Nikolay Bely <belyynikolay@gmail.com>
2019-12-05 02:28:11 +0700

    Add simple xlsx service

---
+++ xlsx_service/main.py
@@ -0,0 +1,34 @@
+import http.server
+import socketserver
+import io
+
+import xlsxwriter
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        output = io.BytesIO()
+
+        workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+        worksheet = workbook.add_worksheet()
+
+        worksheet.write(0, 0, 'Hello, world!')
+
+        workbook.close()
+        output.seek(0)
+
+        # Construct a server response.
+        self.send_response(200)
+        self.send_header('Content-Disposition',
+                         'attachment; filename=test.xlsx')
+        self.send_header('Content-type',
+                         'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        self.end_headers()
+        self.wfile.write(output.read())
+        return
+
+
+print('Server listening on port 8000...')
+httpd = socketserver.TCPServer(('', 8000), Handler)
+httpd.serve_forever()
