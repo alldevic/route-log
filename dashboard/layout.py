fdeb629b316647374a148e7a8d36f17d44e6a2ec
Nikolay Bely <belyynikolay@gmail.com>
2019-12-04 21:12:10 +0700

    Add base map

---
+++ dashboard/layout.py
@@ -0,0 +1,59 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from datetime import datetime as dt
+from plotly import graph_objs as go
+
+base_layout = html.Div(
+    children=[
+        html.Div(
+            className="row",
+            children=[
+                # Column for user controls
+                html.Div(
+                    className="three columns div-user-controls",
+                    children=[
+                        # html.Img(
+                        #     className="logo",
+                        #     src=app.get_asset_url("logo.png")
+                        # ),
+                        html.H2("Маршрутный журнал"),
+                        html.P(
+                            """Выберите день, автомобиль и прочее"""
+                        ),
+                        html.Div(
+                            className="div-for-dropdown",
+                            children=[
+                                dcc.DatePickerSingle(
+                                    id="date-picker",
+                                    min_date_allowed=dt(2019, 4, 1),
+                                    max_date_allowed=dt(2019, 9, 30),
+                                    initial_visible_month=dt(2019, 4, 1),
+                                    date=dt(2019, 4, 1).date(),
+                                    display_format="MMMM D, YYYY",
+                                    style={"border": "0px solid black"},
+                                )
+                            ],
+                        ),
+                        html.Div(
+                            className="div-for-dropdown",
+                            children=[
+                                dcc.Dropdown(
+                                    id="car-dropdown",
+                                    placeholder="Выберите автомобиль",
+                                )
+                            ],
+                        ),
+                    ]
+                ),
+                # Column for app graphs and tables
+                html.Div(
+                    className="nine columns div-for-charts bg-grey",
+                    children=[
+                        dcc.Graph(id="map-graph"),
+                        dcc.Graph(id="data-table"),
+                    ],
+                ),
+            ]
+        )
+    ]
+)
--- dashboard/main.py
+++ dashboard/main.py
@@ -1,5 +1,25 @@
 import dash
+from dash.dependencies import Input, Output
+from plotly import graph_objs as go
+
+from .layout import base_layout
 
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
 )
+
+app.layout = base_layout
+
+
+@app.callback(Output("map-graph", "figure"),
+              [Input("car-dropdown", "value")])
+def init_graph(vl):
+    return go.Figure(
+        data=go.Scattermapbox(),
+        layout=go.Layout(
+            autosize=True,
+            margin=go.layout.Margin(l=0, t=35, r=0, b=35),
+            showlegend=False,
+            mapbox_style="open-street-map",
+        ),
+    )
