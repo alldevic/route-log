3f84ca35373f24c4933a77a50a5fce9ef29b5822
Nikolay Bely <belyynikolay@gmail.com>
2019-12-04 22:01:29 +0700

    Add draft reset zoom button

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,7 +5,10 @@ from plotly import graph_objs as go
 from .layout import base_layout
 
 app = dash.Dash(
-    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+    __name__,
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width"}
+    ],
 )
 
 app.layout = base_layout
@@ -20,6 +23,44 @@ def init_graph(vl):
             autosize=True,
             margin=go.layout.Margin(l=0, t=35, r=0, b=35),
             showlegend=False,
-            mapbox_style="open-street-map",
+            mapbox=dict(
+                style="open-street-map",
+                # center=dict(lat=latInitial, lon=lonInitial),  # 40.7272  # -73.991251
+                # bearing=bearing,
+                # zoom=zoom,
+            ),
+            updatemenus=[
+                dict(
+                    buttons=(
+                        [
+                            dict(
+                                args=[
+                                    {
+                                        "mapbox.zoom": 12,
+                                        "mapbox.center.lon": "87.136101",
+                                        "mapbox.center.lat": "53.757607",
+                                        "mapbox.bearing": 0,
+                                        "mapbox.style": "open-street-map",
+                                    }
+                                ],
+                                label="Reset Zoom",
+                                method="relayout",
+                            )
+                        ]
+                    ),
+                    direction="left",
+                    pad={"r": 0, "t": 0, "b": 0, "l": 0},
+                    showactive=False,
+                    type="buttons",
+                    x=0.45,
+                    y=0.02,
+                    xanchor="left",
+                    yanchor="bottom",
+                    bgcolor="#323130",
+                    borderwidth=1,
+                    bordercolor="#6d6d6d",
+                    font=dict(color="#FFFFFF"),
+                )
+            ],
         ),
     )
