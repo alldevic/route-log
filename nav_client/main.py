71a4fdbf2e8a71ec812aac3c3823c8d3d74c44ad
Nikolay Bely <belyynikolay@gmail.com>
2019-12-06 11:38:43 +0700

    Add fastapi server to nav_client

--- nav_client/main.py
+++ nav_client/main.py
@@ -1,16 +1,56 @@
+from fastapi import FastAPI
 from requests import Session
+from zeep.cache import InMemoryCache
 from requests.auth import HTTPBasicAuth
-from zeep import Client
+import zeep
 from zeep.transports import Transport
-
 import os
 
 env = os.environ
-
+cache = InMemoryCache()
 session = Session()
 session.auth = HTTPBasicAuth(env['SOAP_USER'], env['SOAP_PASS'])
 
-client = Client(wsdl=env['SOAP_WSDL'],
-                transport=Transport(session=session))
+transport = Transport(session=session,
+                      cache=cache)
 
-print(client.service.getAllDevices())
+client = zeep.Client(wsdl=env['SOAP_WSDL'],
+                     transport=transport)
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "world!"}
+
+
+@app.get("/getAllDevices")
+def get_all_devices():
+    return client.service.getAllDevices()
+
+
+@app.get("/getAllDeviceGroups")
+def get_all_device_groups():
+    return client.service.GetAllDeviceGroups()
+
+
+@app.get("/getAllDrivers")
+def get_all_drivers():
+    return client.service.getAllDrivers()
+
+
+@app.get("/getAllGeoZones")
+def get_all_geo_zones():
+    return client.service.getAllGeoZones()
+
+# getAllRoutes(dt from, dt to)
+# @app.get("/getAllDevices")
+# def read_item():
+#     return json.dumps(client.service.getAllDevices())
+
+
+@app.get("/getChannelDescriptors/{device_id}")
+def get_channel_descriptors(device_id: int):
+    return client.service.getChannelDescriptors(device_id)
