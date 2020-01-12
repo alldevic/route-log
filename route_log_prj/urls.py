4d93153be97123a2fe6869a5e4f67456b687d8f2
Nikolay Bely <belyynikolay@gmail.com>
2020-01-13 00:43:27 +0700

    Fix admin naming

--- route_log_prj/urls.py
+++ route_log_prj/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Адмнистрирование маршрутного журнала"
+admin.site.site_title = "Адмнистрирование маршрутного журнала"
+admin.site.index_title = "Маршрутный журнал"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sb/', include('django_sb_admin.urls')),
