ef70ae95e3694f892aab0e09e1591ed4a905f4a4
Nikolay Bely <belyynikolay@gmail.com>
2020-01-21 08:24:31 +0700

    Add draft models

--- route_log/admin.py
+++ route_log/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document, P2Document, RequestDocument, \
+    ReTimeDocument, AdditionalExportDocument, Report
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(P2Document)
+class P2DocumentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RequestDocument)
+class RequestDocumentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ReTimeDocument)
+class ReTimeDocumentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AdditionalExportDocument)
+class AdditionalExportDocumentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    pass
--- route_log/views.py
+++ route_log/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import DocumentForm
+
 
 @login_required
 def index(request):
-    return render(
-        request,
-        'route_log/index.html',
-        context={},
-    )
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = DocumentForm()
+    return render(request, 'route_log/index.html', {
+        'form': form
+    })
