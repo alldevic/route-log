b7543727a35388553762e8cc24b9bab01b372908
Роман Комаров <rom541997@gmail.com>
2020-03-12 17:01:20 +0700

    FEATURE: Add geozones urls

--- nav_client/views.py
+++ nav_client/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from nav_client.serializers import DeviceSerializer
-from nav_client.models import Device, SyncDate
-
+from nav_client.serializers import DeviceSerializer, GeozoneSerializer, NavMtIdSerializer
+from nav_client.models import Device, SyncDate, GeoZone, NavMtId
+from nav_client.filter import GeozoneFilter, NavMtIdFilter
 
 try:
     last_sync_date = SyncDate.objects.last()
@@ -18,3 +18,23 @@ class DeviceListView(generics.ListAPIView):
     queryset = Device.objects.filter(sync_date=last_sync_date)
     serializer_class = DeviceSerializer
     permission_classes = (IsAuthenticated,)
+
+
+class GeozoneListView(generics.ListAPIView):
+    """
+    Список площадок(муниципальных образований)
+    """
+    queryset = GeoZone.objects.all()
+    serializer_class = GeozoneSerializer
+    filterset_class = GeozoneFilter
+    # permission_classes = (IsAuthenticated,)
+
+
+class NavMtIdListView(generics.ListAPIView):
+    """
+    Список площадок из МТ
+    """
+    queryset = NavMtId.objects.all()
+    serializer_class = NavMtIdSerializer
+    filterset_class = NavMtIdFilter
+    # permission_classes = (IsAuthenticated,)
