6feebf3e63ede1db8dea6856693fa22e5fb8c55f
Nikolay Bely <belyynikolay@gmail.com>
2020-03-16 09:17:22 +0700

    Remove  cors

--- route_log_prj/settings.py
+++ route_log_prj/settings.py
@@ -19,7 +19,6 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 ]
 
 THIRD_PARTY_APPS = [
@@ -60,12 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
-
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_METHODS = [ 'DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT', ]
 
 if DEBUG:
     MIDDLEWARE = ['silk.middleware.SilkyMiddleware', ] + MIDDLEWARE
