4b24bf68b7b995022f5c9d7d38ba51edb1b489b8
Роман Комаров <rom541997@gmail.com>
2020-03-19 09:24:13 +0700

    FIX: Deployment

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,9 +4,9 @@ if [[ ${DEBUG} == 'TRUE' ]] || [[ ${DEBUG} == 'True' ]] || [[ ${DEBUG} == '1' ]]
     echo >&2 "Starting debug server..."
     npm i
     npm run serve
-else
-    echo >&2 "Starting prod server..."
-    npm i
-    npm run build
-    npx serve -s dist -l tcp://0.0.0.0:8080
+# else
+#     echo >&2 "Starting prod server..."
+#     npm i
+#     npm run build
+#     npx serve -s dist -l tcp://0.0.0.0:8080
 fi
