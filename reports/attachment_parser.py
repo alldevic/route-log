9fc6c4eabfd9cfe4e4e9b9973d23767b74a5f32a
Nikolay Bely <belyynikolay@gmail.com>
2020-03-26 11:08:39 +0700

    Remove unused navmtids

--- reports/attachment_parser.py
+++ reports/attachment_parser.py
@@ -17,23 +17,13 @@ def parse(file, date, device, container_types):
     all_flats = [x for x in FlatTableRow.objects
                  .filter(device=device, sync_date=sync_date)
                  .prefetch_related('point_value')]
-    mtids = [x for x in NavMtId.objects.filter(sync_date=sync_date)]
+    # mtids = [x for x in NavMtId.objects.filter(sync_date=sync_date)]
     geozones = [x for x in GeoZone.objects.filter(sync_date=sync_date)]
 
     worksheet = xlrd.open_workbook(file_contents=file.read()).sheet_by_index(0)
     for row in worksheet.get_rows():
-        if not check_schedule(row[17].value, date):
-            continue
-
-        geozone = None
-        fl = True
-        for x in mtids:
-            if x.mt_id == int(row[1].value):
-                geozone = x
-                fl = False
-                break
-        if fl or geozone is None:
-            continue
+        # if not check_schedule(row[17].value, date):
+        #     continue
 
         # row14 = str(row[14].value).split(' ')
         fl = True
@@ -44,20 +34,24 @@ def parse(file, date, device, container_types):
         if fl:
             continue
 
+        geozone = None
+        fl = True
+        for x in geozones:
+            if x.mt_id and (x.mt_id == int(row[1].value)):
+                geozone = x
+                report_points = [t for t in x.points.all()]
+                fl = False
+                break
+        if fl or geozone is None:
+            continue
+
         report_row = {}
+        report_row["geozone"] = geozone
         report_row["nav_mt_id"] = geozone.mt_id or None
         report_row["directory"] = geozone.name or "geozone"
-
-        for xx in geozones:
-            if int(xx.nav_id) == int(geozone.nav_id):
-                report_row["geozone"] = xx
-                report_points = [x for x in xx.points.all()]
-                break
-
         report_row["count"] = row[16].value
         report_row["value"] = row[15].value
         report_row["ct_type"] = row[14].value
-
         report_row["time_in"] = None
         report_row["time_out"] = None
         report_row["is_unloaded"] = False
@@ -130,7 +124,9 @@ def check_schedule(schedule, date):
 
 
 def is_days_numbers(prep_list):
-    prep_list = prep_list.split(',')
+    prep_list = [x
+                 for y in prep_list.split(',')
+                 for x in y.split('.')]
 
     for num in prep_list:
         for ch in num:
