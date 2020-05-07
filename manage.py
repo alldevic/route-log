1adac34472d8fe24f0405816014b38791b483d0b
Nikolay Bely <belyynikolay@gmail.com>
2020-05-07 15:19:41 +0700

    Revert "Replace ptvsd via debugpy"
    This reverts commit 1ccbcaacffe3fe74d31d6697d240bfadf5e74b8e.

--- manage.py
+++ manage.py
@@ -18,8 +18,8 @@ def main():
 
     if settings.DEBUG:
         if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-            import debugpy
-            debugpy.listen(('0.0.0.0', 5678))
+            import ptvsd
+            ptvsd.enable_attach(address=('0.0.0.0', 5678))
             print("Attached remote debugger")
     execute_from_command_line(sys.argv)
 
